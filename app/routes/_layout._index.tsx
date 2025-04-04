74627c0cbc42f4a435f8cd49edf7fdd87a3f8259
abjunior92 <junior.berselli@gmail.com>
2025-04-04 17:52:25 +0200

    fix center header title

--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -28,7 +28,7 @@ export default function Index() {
 		<div className="flex h-screen items-center justify-center">
 			<div className="flex flex-col items-center gap-8">
 				<header className="flex flex-col items-center">
-					<h1 className="leading text-3xl font-bold text-gray-800 dark:text-gray-100">
+					<h1 className="leading text-center text-3xl font-bold text-gray-800 dark:text-gray-100">
 						Welcome to Playgrounds 🏀
 					</h1>
 				</header>
