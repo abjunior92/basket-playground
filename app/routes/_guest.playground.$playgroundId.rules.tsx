43098a870ca5bb1e993238e7917e94f405dd7d80
abjunior92 <junior.berselli@gmail.com>
2025-06-27 11:12:55 +0200

    fix height for rules

--- app/routes/_guest.playground.$playgroundId.rules.tsx
+++ app/routes/_guest.playground.$playgroundId.rules.tsx
@@ -20,26 +20,28 @@ export default function Rules() {
 				backLink={`/playground/${params.playgroundId}/menu`}
 				icon={<BookOpenText />}
 			/>
-			<div className="space-y-2 pt-4">
-				<p>Il regolamento è fondamentale per poter partecipare al torneo.</p>
-				<p>
-					È importante comprendere il contesto e il luogo in cui si gioca, per
-					poter giocare in maniera corretta e rispettosa.
-				</p>
-				<p>
-					Leggere il regolamento aiuta a capire le regole e le procedure da
-					seguire, per evitare di commettere errori che possono portare a
-					penalizzazioni o esclusioni dal torneo.
-				</p>
-			</div>
+			<div className="flex h-[calc(100%-60px)] flex-col justify-between md:h-1/2">
+				<div className="space-y-2 pt-4">
+					<p>Il regolamento è fondamentale per poter partecipare al torneo.</p>
+					<p>
+						È importante comprendere il contesto e il luogo in cui si gioca, per
+						poter giocare in maniera corretta e rispettosa.
+					</p>
+					<p>
+						Leggere il regolamento aiuta a capire le regole e le procedure da
+						seguire, per evitare di commettere errori che possono portare a
+						penalizzazioni o esclusioni dal torneo.
+					</p>
+				</div>
 
-			<div className="flex h-full flex-col items-center justify-center">
-				<Link to="/regolamento.docx" target="_blank" rel="noreferrer">
-					<Button>
-						<BookOpenText />
-						Scarica/Apri regolamento
-					</Button>
-				</Link>
+				<div className="flex flex-col items-center justify-center">
+					<Link to="/regolamento.docx" target="_blank" rel="noreferrer">
+						<Button>
+							<BookOpenText />
+							Scarica/Apri regolamento
+						</Button>
+					</Link>
+				</div>
 			</div>
 		</div>
 	)
