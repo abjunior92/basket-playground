22a970c611be84c55766b693e04df1ecb007a708
abjunior92 <junior.berselli@gmail.com>
2026-04-08 15:28:05 +0200

    Refactor layout components: Replace divs with main elements for better semantic structure, and enhance header sections with icons for improved visual clarity in the menu and palmares displays.

--- app/routes/_guest.playground.$playgroundId.menu.tsx
+++ app/routes/_guest.playground.$playgroundId.menu.tsx
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
 import { Link, useLoaderData } from '@remix-run/react'
-import { BookOpenText, CalendarCog, Medal, Trophy } from 'lucide-react'
+import { Award, BookOpenText, CalendarCog, Medal, Trophy } from 'lucide-react'
 import invariant from 'tiny-invariant'
 
 const prisma = new PrismaClient()
@@ -33,7 +33,7 @@ export default function Index() {
 	const { playground, palmaresList } = useLoaderData<typeof loader>()
 
 	return (
-		<div className="flex h-screen items-center justify-center">
+		<main className="flex h-screen items-center justify-center">
 			<div className="flex flex-col items-center gap-8">
 				<header className="flex flex-col items-center">
 					<h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
@@ -54,14 +54,21 @@ export default function Index() {
 						))}
 					</ul>
 				</nav>
-				<h2 className="mt-4 text-xl font-bold">Palmares</h2>
+				<h2 className="mt-4 text-xl font-bold">
+					<div className="flex items-center space-x-2">
+						<Award className="h-5 w-5" />
+						<span>Palmares</span>
+					</div>
+				</h2>
 				<nav className="flex flex-col items-center justify-center">
 					<ul className="space-y-4">
 						{palmaresList.length > 0 ? (
 							palmaresList.map(({ year }) => (
 								<li key={year} className="nav-button">
 									<Link to={`/playground/${playground.id}/palmares/${year}`}>
-										Anno {year}
+										<div className="flex items-center space-x-2">
+											<span>Anno {year}</span>
+										</div>
 									</Link>
 								</li>
 							))
@@ -71,7 +78,7 @@ export default function Index() {
 					</ul>
 				</nav>
 			</div>
-		</div>
+		</main>
 	)
 }
 
--- app/routes/_layout._index.tsx
+++ app/routes/_layout._index.tsx
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { type LoaderFunctionArgs, type MetaFunction } from '@remix-run/node'
 import { Form, Link, useLoaderData } from '@remix-run/react'
-import { Award, LogOut, Trophy, UserCog } from 'lucide-react'
+import { Award, Cog, LogOut, Trophy, UserCog } from 'lucide-react'
 import { Button } from '~/components/ui/button'
 import { checkUserIsLoggedIn } from '~/utils/helpers'
 
@@ -30,14 +30,19 @@ export default function Index() {
 	const { playgrounds, palmaresList } = useLoaderData<typeof loader>()
 
 	return (
-		<div className="flex h-screen items-start justify-center md:items-center">
+		<main className="flex h-screen items-start justify-center md:items-center">
 			<div className="flex flex-col items-center gap-8">
 				<header className="flex flex-col items-center">
 					<h1 className="leading text-center text-3xl font-bold text-gray-800 dark:text-gray-100">
 						Welcome to Playgrounds 🏀
 					</h1>
 				</header>
-				<h2 className="mt-4 text-xl font-bold">Menù Admin</h2>
+				<h2 className="mt-4 text-xl font-bold">
+					<div className="flex items-center space-x-2">
+						<Cog className="h-5 w-5" />
+						<span>Menù Admin</span>
+					</div>
+				</h2>
 				<nav className="flex flex-col items-center justify-center">
 					<ul className="space-y-4">
 						{resources.map(({ href, text, icon }) => (
@@ -52,7 +57,12 @@ export default function Index() {
 						))}
 					</ul>
 				</nav>
-				<h2 className="mt-4 text-xl font-bold">Lista Tornei</h2>
+				<h2 className="mt-4 text-xl font-bold">
+					<div className="flex items-center space-x-2">
+						<Trophy className="h-5 w-5" />
+						<span>Lista Tornei</span>
+					</div>
+				</h2>
 				<nav className="flex flex-col items-center justify-center">
 					<ul className="space-y-4">
 						{playgrounds.length > 0 ? (
@@ -60,7 +70,6 @@ export default function Index() {
 								<li key={id} className="nav-button">
 									<Link to={`/playground/${id}`}>
 										<div className="flex items-center space-x-2">
-											<Award className="h-5 w-5" />
 											<span>{name}</span>
 										</div>
 									</Link>
@@ -71,13 +80,22 @@ export default function Index() {
 						)}
 					</ul>
 				</nav>
-				<h2 className="mt-4 text-xl font-bold">Palmares</h2>
+				<h2 className="mt-4 text-xl font-bold">
+					<div className="flex items-center space-x-2">
+						<Award className="h-5 w-5" />
+						<span>Palmares</span>
+					</div>
+				</h2>
 				<nav className="flex flex-col items-center justify-center">
 					<ul className="space-y-4">
 						{palmaresList.length > 0 ? (
 							palmaresList.map(({ year }) => (
 								<li key={year} className="nav-button">
-									<Link to={`/palmares/${year}`}>Anno {year}</Link>
+									<Link to={`/palmares/${year}`}>
+										<div className="flex items-center space-x-2">
+											<span>Anno {year}</span>
+										</div>
+									</Link>
 								</li>
 							))
 						) : (
@@ -93,7 +111,7 @@ export default function Index() {
 					</Button>
 				</Form>
 			</div>
-		</div>
+		</main>
 	)
 }
 
